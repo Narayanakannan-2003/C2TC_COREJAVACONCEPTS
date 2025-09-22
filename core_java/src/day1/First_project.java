14cd439d0d7ab25fe8ac8666992a96bd0a136fdc
Narayanan.G <Narayanan.G@172.16.0.126>
2025-09-22 18:54:26 +0530

    commit

--- core_java/src/day1/First_project.java
+++ core_java/src/day1/First_project.java
@@ -1,8 +1,21 @@
 package day1;
+import java.util.*;
 
 public class First_project {
 	public static void main(String args[]) {
-		System.out.print("hello");
+		Scanner scan= new Scanner(System.in);
+		String s1=scan.nextLine();
+		String s2="";
+		for(int i=s1.length()-1;i>=0;i--) {
+			s2=s2+s1.charAt(i);
+		}
+		System.out.print(s2);
+		if(s2.equals(s1)) {
+			System.out.print(" " +"this is palindrom");
+		}
+		else {
+			System.out.print(" "+"not a palindrom");
+		}
 	}
 
 }
