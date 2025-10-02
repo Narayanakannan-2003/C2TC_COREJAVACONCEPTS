f857b5cca29d898c1f2ec267f5d6f1c3959f7768
Narayanan.G <Narayanan.G@LAPTOP-GFIKT0GL>
2025-10-02 10:09:24 +0530

    commit

---
+++ core_java/src/com/tnsif/DAY5/multilevelinheritance/package-info.java
@@ -0,0 +1,1 @@
+package com.tnsif.DAY5.multilevelinheritance;
---
+++ core_java/src/com/tnsif/DAY5/multilevelinheritance/texteditor/Notepad.java
@@ -0,0 +1,28 @@
+package com.tnsif.DAY5.multilevelinheritance.texteditor;
+
+public class Notepad {
+	protected String content;
+
+    public Notepad() {
+        this.content = "";
+    }
+
+    public void write(String text) {
+        content += text; //content = content+text
+    }
+
+    public void displayContent() {
+        System.out.println("Notepad Content: " + content);
+    }
+    
+    
+    /*
+     * returntype funcName()
+     * {
+     * 		//statement
+     * }
+     * 
+     * */
+}
+
+
